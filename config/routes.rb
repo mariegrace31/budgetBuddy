f3510aed705be02416607bbe17107e95c3df3778
mariegrace <mariegracebmg@gmail.com>
2023-10-31 18:26:59 +0200

    Add exchanges views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'exchanges/index'
+  get 'exchanges/new'
+  get 'exchanges/create'
+  get 'categories/index'
+  get 'categories/home'
+  get 'categories/new'
+  get 'categories/create'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

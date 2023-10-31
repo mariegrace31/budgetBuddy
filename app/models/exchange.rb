624dd6b5b6f5b858b996e72879fdd3f0923dcdc6
mariegrace <mariegracebmg@gmail.com>
2023-10-31 21:27:12 +0200

    Add models validations

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,4 +1,7 @@
 class Exchange < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :exchange_groups, dependent: :destroy
+
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
 
   has_many :exchanges, class_name: 'exchange', foreign_key: 'author_id', dependent: :destroy
   has_many :groups, dependent: :destroy
+
+  validates :name, presence: true
 end

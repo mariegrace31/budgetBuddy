fd4830be1e1a3d9e9f646b5e21e44b8e8c566949
mariegrace <mariegracebmg@gmail.com>
2023-11-01 13:29:20 +0200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_31_152436) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_01_112713) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,8 +31,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_31_152436) do
   end
 
   create_table "groups", force: :cascade do |t|
-    t.string "name"
-    t.string "icon"
+    t.string "name", null: false
+    t.string "icon", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false

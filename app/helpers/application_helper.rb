701a2ba43eb42a1e4c87cb16bffdb9a86a83eeca
mariegrace <mariegracebmg@gmail.com>
2023-11-01 13:48:35 +0200

    Add another helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def format_large_number(number)
+    return number.to_s if number <= 0
+
+    suffixes = {
+      0 => '', # No suffix for numbers less than 1,000
+      1 => 'K', # 'Thousand' suffix
+      2 => 'M', # 'Million' suffix
+      3 => 'B' # 'Billion' suffix, and so on...
+      # Add more suffixes as needed for larger numbers
+    }
+
+    order = (Math.log10(number) / 3).to_i
+    suffix = suffixes[order]
+
+    formatted_number = if order.positive?
+        # Convert to float and remove trailing ".0"
+                         (number / (10**(order * 3).to_f)).round(1).to_s.sub(/\.0$/, '')
+                       else
+                         number.to_s
+                       end
+
+    "#{formatted_number}#{suffix}"
+  end
 end
